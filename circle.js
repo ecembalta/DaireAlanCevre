e5ebc030acb84190a325ae91e61bbbf0702d083e
unknown <ecemnurbalta@gmail.com>
2021-12-17 14:50:45 +0300

    first commit

---
+++ circle.js
@@ -0,0 +1,14 @@
+module.exports = {
+  circleArea,
+  circleCircumference,
+};
+
+function circleArea(r) {
+  let area = Math.PI * r * r;
+  console.log('Dairenin Alanı:', area);
+}
+
+function circleCircumference(r) {
+  let circumference = Math.PI * 2 * 2;
+  console.log('Dairenin Çevresi:', circumference);
+}
